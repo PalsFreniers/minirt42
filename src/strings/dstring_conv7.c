37c87d280d6fd3eb0b4f0802016b5121a024b427
Strong214356 <tidiandelage1@gmail.com>
2024-09-10 23:57:21 +0200

    fixed infinite loop for negative and normed (wierd code dont look)

--- src/strings/dstring_conv7.c
+++ src/strings/dstring_conv7.c
@@ -12,34 +12,30 @@ static struct s_string	destroy_zero(struct s_string *a)
 	return (string_zero());
 }
 
+void	add_start_zero(struct s_string *ret, float fpart, int p)
+{
+	string_push(ret, '.');
+	while (fpart < (int)(p / 10) && string_error(false, 0) == STRING_SUCCESS)
+	{
+		fpart *= 10;
+		string_push(ret, '0');
+	}
+}
+
 struct s_string	string_from_float(float value, int a_dot)
 {
 	struct s_string	ret;
 	float			fpart;
 	struct s_string	dec;
-	int				p;
-	int				app;
 
 	ret = string_from_sqword((int64_t)value);
 	if (string_error(false, 0) != STRING_SUCCESS)
 		return ((struct s_string){0});
-	fpart = value - (int64_t)value;
-	if (fpart == 0)
-		return (ret);
-	p = pow(10, a_dot);
-	fpart *= p;
+	fpart = (value - (int64_t)value) * pow(10, a_dot);
+	fpart *= -1 + 2 * (!(value < 0 && (int)value == 0));
 	if ((size_t)fpart == 0)
 		return (ret);
-	app = 0;
-	string_push(&ret, '.');
-	while (fpart < (int)(p / 10))
-	{
-		fpart *= 10;
-		app++;
-		string_push(&ret, '0');
-	}
-	app = pow(10, app);
-	fpart /= app;
+	add_start_zero(&ret, fpart, pow(10, a_dot));
 	if (string_error(false, 0) != STRING_SUCCESS)
 		return (destroy_zero(&ret));
 	dec = string_from_uqword(fpart);
@@ -47,9 +43,8 @@ struct s_string	string_from_float(float value, int a_dot)
 		return (destroy_zero(&ret));
 	string_append(&ret, dec);
 	string_destroy(&dec);
-	if (fpart)
-		if (string_error(false, 0) != STRING_SUCCESS)
-			return (destroy_zero(&ret));
+	if (string_error(false, 0) != STRING_SUCCESS)
+		return (destroy_zero(&ret));
 	if (value < 0 && (int)value == 0)
 		string_push_front(&ret, '-');
 	return (ret);
