354be5714d7363eed51c038528ee13a3003cebc4
Marwan Amine <maamine@2E5.42angouleme.fr>
2024-10-18 23:50:06 +0200

    Developped raycast from camera and collision detection logic

--- src/render/collision.h
+++ src/render/collision.h
@@ -1,8 +1,18 @@
 #ifndef COLLISION_H
 # define COLLISION_H
 
+
+# include <mlx/mmlx.h>
+
 # include <math/ray.h>
 # include <stdbool.h>
+
+typedef struct s_collision
+{
+	struct s_vec3	position;
+	struct s_vec3	normal;
+	struct s_object	object;
+}					t_collision;
 
 bool	sphere_collide_function(struct s_ray ray, struct s_sphere *sphere,
 			struct s_vec3 position, struct s_vec3 normal);
--- src/render/loop_render.c
+++ src/render/loop_render.c
@@ -6,42 +6,81 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 03:53:57 by maamine           #+#    #+#             */
-/*   Updated: 2024/10/13 04:16:23 by maamine          ###   ########.fr       */
+/*   Updated: 2024/10/18 23:48:53 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include "collision.h"
+#include <math/la.h>
 
 /**
  * 	loop through x, y:
- * 		draw_line			--> not hard, only depends on x, y and FOV
- * 		get_intersection	--> moves and rotates line, check with objects
+ * 		shoot_ray_from_camera
+ * 		get_collision			--> gets the nearest collision
  * 
- * 	get_intersection:
+ * 	shoot_ray_from_camera:
+ * 		(FOV, x, y) --> ray
+ * 		ray --> rotate from camera referential to world referential
+ * 
+ * 	get_collision:
  * 		loop through objects:
- * 			if check_intersection:
- * 				if nearer and valid:
- * 					inters = tmp_inters
+ * 			if check_collision and nearest and valid:
+ * 				collision = tmp_collision
  */
+
+t_vec3	shoot_ray_from_camera(struct s_mlx *mlx, int x, int y)
+{
+	t_vec3	ray;
+
+	// ray.x = math_formula;
+	// ray.y = math_formula;
+	// ray.z = math_formula;
+	// ray = rotate(ray, camera);
+}
+
+t_collision	get_collision(struct s_mlx *mlx, struct s_vec3 ray)
+{
+	t_collision		collision;
+	t_collision		tmp_collision;
+	struct s_object	*object;
+	float			dist;
+	float			tmp_dist;
+
+	// object = first object of the scene;
+	// dist = +inf;
+	while (object)
+	{
+		if (test_collision(object, ray, &tmp_collision) == 1)
+			tmp_dist = get_dist(tmp_collision);
+			if (tmp_dist < dist)
+			{
+				collision = tmp_collision;
+				dist = tmp_dist;
+			}
+	// 	object = next object;
+	}
+	return (collision);
+}
 
 void	loop_render(struct s_mlx *mlx)
 {
-	// int	x;
-	// int	y;
-	// struct s_vec3	line;
-	// struct s_inters	inters;
+	int			x;
+	int			y;
+	t_vec3		ray;
+	t_collision	collision;
 
-	// y = 0;
-	// while (y < 1000)
-	// {
-	// 	x = 0;
-	// 	while (x < 1000)
-	// 	{
-	// 		line = draw_line(mlx, x, y);
-	// 		inters = get_inters(mlx, line);
-	// 		draw_pixel(inters);
-	// 		x++;
-	// 	}
-	// 	y++;
-	// }
+	y = 0;
+	while (y < 1000)
+	{
+		x = 0;
+		while (x < 1000)
+		{
+			ray = shoot_ray_from_camera(mlx, x, y);
+			collision = get_collision(mlx, ray);
+	// 		draw_pixel(collision);
+			x++;
+		}
+		y++;
+	}
 }
