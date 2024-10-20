d4a49a7ce8f6407f30b60c708cb56d65ecbcefe9
Strong214356 <tidiandelage1@gmail.com>
2024-10-20 18:19:53 +0200

    adding vec3 map 2pi function and adding rotation matrix helper function apply and get rotation matrix

--- src/math/la.h
+++ src/math/la.h
@@ -44,7 +44,18 @@ float				vec3_lenght(t_vec3 a);
 float				vec3_dot(t_vec3 a, t_vec3 b);
 float				vec3_sum(t_vec3 a);
 
+t_vec3 vec3_map_2pi(t_vec3 a);
+
 t_mat3				mat3_mul(t_mat3 a, t_mat3 b);
 t_vec3 mat3_apply(t_mat3 a, t_vec3 b);
+
+t_mat3 mat3_zero();
+t_mat3 mat3_identity();
+
+t_mat3 rotation_matrix_x(float angle);
+t_mat3 rotation_matrix_y(float angle);
+t_mat3 rotation_matrix_z(float angle);
+
+t_mat3 get_rotation_matrix(t_vec3 angle);
 
 #endif // LA_H
