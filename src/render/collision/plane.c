2c701732f159ff07674152803603343ca9993e9b
Tidian Delage <tdelage@3C6.42angouleme.fr>
2024-12-13 03:35:10 +0100

    fixing plane collisions

--- src/render/collision/plane.c
+++ src/render/collision/plane.c
@@ -9,18 +9,26 @@
 bool	plane_collide_function(struct s_ray ray, struct s_plane *plane,
 		t_collision *coll, t_matrices *m)
 {
+	t_vec3	norm;
+	float	determinant;
+	float	distance;
+
 	(void)m;
-        t_vec3 norm = vec3_normalise(plane->normal);
-        float determinant = ft_abs(vec3_dot(ray.direction, norm));
-	if (determinant <= 0)
+	norm = vec3_normalise(plane->normal);
+	determinant = vec3_dot(ray.direction, norm);
+	if (determinant == 0)
+		return (false);
+	distance = vec3_dot(vec3_sub(plane->base.position, ray.origin), norm)
+		/ determinant;
+	if (distance < 0)
 		return (false);
 	if (coll)
 	{
-		float distance = vec3_dot(vec3_sub(plane->base.position, ray.origin), norm) / determinant;
 		coll->dist = distance;
-		coll->position = vec3_add(ray.origin, vec3_scalar_mul(ray.direction, distance));
+		coll->position = vec3_add(ray.origin, vec3_scalar_mul(ray.direction,
+					distance));
 		coll->normal = norm;
-		coll->object = (struct s_object *) plane;
+		coll->object = (struct s_object *)plane;
 	}
 	return (true);
 }
