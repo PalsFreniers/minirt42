ad239c852d754445d6aeacffe864c242d1ad9442
Marwan Amine <maamine@1G1.42angouleme.fr>
2025-01-27 19:43:34 +0100

    New shading and float colors

--- src/render/color.c
+++ src/render/color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marwan <marwan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 19:30:16 by maamine           #+#    #+#             */
-/*   Updated: 2025/01/27 01:53:15 by marwan           ###   ########.fr       */
+/*   Updated: 2025/01/27 19:40:45 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ struct s_object *get_map_light(struct s_scene *scene) {
                         return scene->map_objects[x];
                 x++;
         }
-        return NULL;
+        return (NULL);
 }
 
 struct s_object *get_real_light(struct s_scene *scene) {
@@ -31,92 +31,100 @@ struct s_object *get_real_light(struct s_scene *scene) {
                         return scene->objects[x];
                 x++;
         }
-        return NULL;
+        return (NULL);
 }
 
-mlx_color	rgb_to_rgba(t_color color)
+t_render_color	render_color_black(void)
 {
-	mlx_color	acolor;
-
-	acolor.a = 0XFF;
-	acolor.r = color.r;
-	acolor.g = color.g;
-	acolor.b = color.b;
-	return (acolor);
+	return ((t_render_color)
+	{
+		.r = 0.0f,
+		.g = 0.0f,
+		.b = 0.0f,
+	});
 }
 
-mlx_color	mul_acolor(mlx_color color, float f)
+mlx_color	rgb_to_mlx_color(union u_color color)
 {
-	color.r *= f;
-	if (color.r > 0XFF)
-		color.r = 0XFF;
-	color.g *= f;
-	if (color.g > 0XFF)
-		color.g = 0XFF;
-	color.b *= f;
-	if (color.b > 0XFF)
-		color.b = 0XFF;
+	mlx_color	ret_color;
+
+	ret_color.a = 0xFF;
+	ret_color.r = color.r;
+	ret_color.g = color.g;
+	ret_color.b = color.b;
+	return (ret_color);
+}
+
+t_render_color	rgb_to_render_color(union u_color color)
+{
+	t_render_color	ret_color;
+
+	ret_color.r = color.r;
+	ret_color.g = color.g;
+	ret_color.b = color.b;
+	return (ret_color);
+}
+
+mlx_color	render_to_mlx_color(t_render_color color)
+{
+	mlx_color	ret_color;
+
+	ret_color.a = 0xFF;
+	if (color.r > 0xFF)
+		ret_color.r = 0xFF;
+	else
+		ret_color.r = color.r;
+	if (color.g > 0xFF)
+		ret_color.g = 0xFF;
+	else
+		ret_color.g = color.g;
+	if (color.b > 0xFF)
+		ret_color.b = 0xFF;
+	else
+		ret_color.b = color.b;
+	return (ret_color);
+}
+
+t_render_color	filter_color(t_render_color color, t_render_color filter)
+{
+	t_render_color	ret_color;
+
+	ret_color.r = (color.r * filter.r) / 0xFF;
+	ret_color.g = (color.g * filter.g) / 0xFF;
+	ret_color.b = (color.b * filter.b) / 0xFF;
+	return (ret_color);
+}
+
+t_render_color	color_scal_mul(t_render_color color, float f)
+{
+	t_render_color	ret_color;
+
+	ret_color.r = color.r * f;
+	ret_color.g = color.g * f;
+	ret_color.b = color.b * f;
+	return (ret_color);
+}
+
+t_render_color	color_add(t_render_color a, t_render_color b)
+{
+	t_render_color	color;
+
+	color.r = a.r + b.r;
+	color.g = a.g + b.g;
+	color.b = a.b + b.b;
 	return (color);
-}
-
-mlx_color	filter_acolor(mlx_color light, mlx_color filter)
-{
-	mlx_color	color;
-
-	color.a = 0XFF;
-	color.r = (light.r * filter.r) / 0XFF;
-	color.g = (light.g * filter.g) / 0XFF;
-	color.b = (light.b * filter.b) / 0XFF;
-	return (color);
-}
-
-mlx_color	blend_color(mlx_color a, mlx_color b, float ratio)
-{
-	mlx_color	blend;
-
-	blend.a = 0xFF;
-	blend.r = (1 - ratio) * a.r + ratio * b.r;
-	blend.g = (1 - ratio) * a.g + ratio * b.g;
-	blend.b = (1 - ratio) * a.b + ratio * b.b;
-	return (blend);
-}
-
-mlx_color	blend_color_safe(mlx_color a, mlx_color b, float ratio)
-{
-	mlx_color	blend;
-	int			tmp;
-
-	blend.a = 0xFF;
-	tmp = (1 - ratio) * a.r + ratio * b.r;
-	if (tmp <= 0XFF)
-		blend.r = tmp;
-	else
-		blend.r = 0XFF;
-	tmp = (1 - ratio) * a.g + ratio * b.g;
-	if (tmp <= 0XFF)
-		blend.g = tmp;
-	else
-		blend.g = 0XFF;
-	tmp = (1 - ratio) * a.b + ratio * b.b;
-	if (tmp <= 0XFF)
-		blend.b = tmp;
-	else
-		blend.b = 0XFF;
-	return (blend);
 }
 
 mlx_color	get_light_color(struct s_mlx *mlx)
 {
-	mlx_color	color;
-
-	color = rgb_to_rgba(get_real_light(&mlx->scene)->color);
-	return (color);
+	return (rgb_to_mlx_color(get_real_light(&mlx->scene)->color));
 }
 
-static bool	is_lit(struct s_mlx *mlx, t_collision *starting_point, t_vec3 light_dir)
+static bool
+is_lit(struct s_mlx *mlx, t_collision *starting_point, t_vec3 light_dir)
 {
 	t_ray		ray;
-	t_collision	new_collision;
+	t_collision	new_coll;
 	float		light_dist;
 	size_t		i;
 
@@ -127,74 +135,62 @@ static bool	is_lit(struct s_mlx *mlx, t_collision *starting_point, t_vec3 light_
 	while (i < mlx->scene.len)
 	{
 		if (mlx->scene.map_objects[i] != starting_point->object
-			&& test_collision(mlx, mlx->scene.map_objects[i], &ray, &new_collision)
-			&& new_collision.dist > 0 && new_collision.dist < light_dist)
+			&& test_collision(mlx, mlx->scene.map_objects[i], &ray, &new_coll)
+			&& new_coll.dist > 0 && new_coll.dist < light_dist)
 			return (false);
 		i++;
 	}
 	return (true);
 }
 
-static mlx_color	lambert_shading(t_collision *coll, t_vec3 coll_to_light)
+static t_render_color	lambert(t_collision *coll, t_vec3 coll_to_light)
 {
-	mlx_color	color;
-	float		light_per_area;
-	float		diffuse_coeff;
-	float		light_amount;
+	float	light_per_area;
+	float	diffuse_coeff;
+	float	light_amount;
 
 	light_per_area = vec3_dot(coll->normal, vec3_normalise(coll_to_light));
 	if (light_per_area <= 0)
-	{
-		color.rgba = 0X000000FF;
-		return (color);
-	}
+		return (render_color_black());
 	diffuse_coeff = 1.0f;
 	// light_amount = diffuse_coeff * light_per_area / vec3_lenght_sq(coll_to_light);
 	// light_amount = diffuse_coeff / vec3_lenght_sq(coll_to_light);
 	light_amount = diffuse_coeff * light_per_area;
-	color = mul_acolor(rgb_to_rgba(coll->object->color), light_amount);
-	return (color);
+	return (color_scal_mul(rgb_to_render_color(coll->object->color),
+				light_amount));
 }
 
-static mlx_color	blinn_phong_shading(t_collision *coll, t_vec3 coll_to_light)
+static t_render_color	blinn_phong(t_collision *coll, t_vec3 coll_to_light)
 {
-	mlx_color	color;
-	t_vec3		reflect_normal;
-	float		light_per_area;
-	float		diffuse_coeff;
-	float		light_amount;
+	t_vec3			reflect_normal;
+	float			light_per_area;
+	float			diffuse_coeff;
+	float			light_amount;
 
-	reflect_normal = vec3_sub(vec3_normalise(coll_to_light), coll->ray.direction);
-	reflect_normal = vec3_normalise(reflect_normal);
+	reflect_normal = vec3_normalise(vec3_sub(vec3_normalise(coll_to_light),
+				coll->ray.direction));
 	light_per_area = vec3_dot(coll->normal, reflect_normal);
 	light_per_area = light_per_area * light_per_area * light_per_area;
 	if (light_per_area <= 0)
-	{
-		color.rgba = 0X000000FF;
-		return (color);
-	}
+		return (render_color_black());
 	diffuse_coeff = 1.0f;
 	// light_amount = diffuse_coeff * light_per_area / vec3_lenght_sq(coll_to_light);
 	// light_amount = diffuse_coeff / vec3_lenght_sq(coll_to_light);
 	light_amount = diffuse_coeff * light_per_area;
-	color = mul_acolor(rgb_to_rgba(coll->object->color), light_amount);
-	return (color);
+	return (color_scal_mul(rgb_to_render_color(coll->object->color),
+				light_amount));
 }
 
-mlx_color	lit_color(struct s_mlx *mlx, t_collision *collision)
+t_render_color	lit_color(struct s_mlx *mlx, t_collision *collision)
 {
-	mlx_color	color;
-	t_vec3		coll_to_light;
+	t_vec3	coll_to_light;
 
-	coll_to_light = vec3_sub(get_map_light(&mlx->scene)->position, collision->position);
+	coll_to_light = vec3_sub(get_map_light(&mlx->scene)->position,
+			collision->position);
 	if (!is_lit(mlx, collision, coll_to_light))
-	{
-		color.rgba = 0X000000FF;
-		return (color);
-	}
-	// color = rgb_to_rgba(collision->object->color);
-	// color = lambert_shading(collision, coll_to_light);
-	color = blend_color(lambert_shading(collision, coll_to_light),
-			blinn_phong_shading(collision, coll_to_light), 0.5f);
-	return (color);
+		return (render_color_black());
+	return (
+		color_add(
+			color_scal_mul(lambert(collision, coll_to_light), 0.5f),
+			color_scal_mul(blinn_phong(collision, coll_to_light), 0.5f)));
 }
--- src/render/render.h
+++ src/render/render.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marwan <marwan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 04:16:46 by maamine           #+#    #+#             */
-/*   Updated: 2025/01/27 01:53:21 by marwan           ###   ########.fr       */
+/*   Updated: 2025/01/27 18:38:52 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,24 @@
 # include <mlx/mmlx.h>
 # include <render/collision.h>
 
+typedef struct s_render_color
+{
+	float	r;
+	float	g;
+	float	b;
+}			t_render_color;
+
 void			loop_render(struct s_mlx *mlx);
 
-mlx_color		rgb_to_rgba(t_color color);
-mlx_color		filter_acolor(mlx_color light, mlx_color filter);
-mlx_color		blend_color(mlx_color a, mlx_color b, float ratio);
-mlx_color		blend_color_safe(mlx_color a, mlx_color b, float ratio);
+t_render_color	render_color_black(void);
+mlx_color		rgb_to_mlx_color(union u_color color);
+t_render_color	rgb_to_render_color(union u_color color);
+mlx_color		render_to_mlx_color(t_render_color color);
+t_render_color	filter_color(t_render_color color, t_render_color filter);
+t_render_color	color_scal_mul(t_render_color color, float f);
+t_render_color	color_add(t_render_color a, t_render_color b);
 mlx_color		get_light_color(struct s_mlx *mlx);
-mlx_color		lit_color(struct s_mlx *mlx, t_collision *collision);
+t_render_color	lit_color(struct s_mlx *mlx, t_collision *collision);
 struct s_object	*get_map_light(struct s_scene *scene);
 struct s_object *get_real_light(struct s_scene *scene);
 
