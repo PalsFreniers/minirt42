349782d0707cbfc830339d4825037a4bb38926cf
Marwan Amine <maamine@2F3.42angouleme.fr>
2025-01-29 22:18:39 +0100

    Sphere small correction

--- src/render/collision/sphere.c
+++ src/render/collision/sphere.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 06:17:56 by tdelage           #+#    #+#             */
-/*   Updated: 2025/01/29 18:26:26 by maamine          ###   ########.fr       */
+/*   Updated: 2025/01/29 22:12:24 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,16 @@ bool	sphere_collide(struct s_ray *ray, struct s_sphere *sphere,
 	float		rtsp_norm_squarred;
 	float		ray_dot_rtsp;
 
-	ray_to_sphere = vec3_sub(ray->origin, sphere->base.position);
+	ray_to_sphere = vec3_sub(sphere->base.position, ray->origin);
 	ray_dot_rtsp = vec3_dot(ray->direction, ray_to_sphere);
 	rtsp_norm_squarred = vec3_lenght_sq(ray_to_sphere);
-	determinant = ray_dot_rtsp * ray_dot_rtsp - (rtsp_norm_squarred - radius
-			* radius);
+	determinant = ray_dot_rtsp * ray_dot_rtsp
+		- (rtsp_norm_squarred - radius * radius);
 	if (determinant < 0)
 		return (false);
 	if (coll)
 	{
-		coll->dist = (-ray_dot_rtsp) - sqrtf(determinant);
+		coll->dist = ray_dot_rtsp - sqrtf(determinant);
 		coll->position = vec3_add(ray->origin,
 				vec3_scal_mul(ray->direction, coll->dist));
 		coll->normal = vec3_normalise(vec3_sub(coll->position,
