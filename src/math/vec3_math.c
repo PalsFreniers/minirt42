536d8df9450edbf8c86a0e93250e559182554f77
Marwan Amine <maamine@3B4.42angouleme.fr>
2025-02-03 00:13:36 +0100

    Slight math rework

--- src/math/vec3_math.c
+++ src/math/vec3_math.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 02:32:23 by maamine           #+#    #+#             */
-/*   Updated: 2025/01/30 02:40:54 by maamine          ###   ########.fr       */
+/*   Updated: 2025/02/03 00:04:30 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,23 @@ t_vec3	vec3_scal_mul(t_vec3 a, float f)
 	return (vec3_new(f * a.x, f * a.y, f * a.z));
 }
 
+float	vec3_sum(t_vec3 a)
+{
+	return (a.x + a.y + a.z);
+}
+
+float	vec3_lenght_sq(t_vec3 a)
+{
+	const t_vec3	tmp = vec3_mul(a, a);
+
+	return (vec3_sum(tmp));
+}
+
+float	vec3_lenght(t_vec3 a)
+{
+	return (sqrtf(vec3_lenght_sq(a)));
+}
+
 t_vec3	vec3_normalise(t_vec3 a)
 {
 	float	len;
@@ -25,21 +42,4 @@ t_vec3	vec3_normalise(t_vec3 a)
 	if (len == 0)
 		return (vec3_zero());
 	return (vec3_div(a, vec3_new_from_one(len)));
-}
-
-float	vec3_lenght(t_vec3 a)
-{
-	const t_vec3	tmp = vec3_mul(a, a);
-
-	return (sqrtf(tmp.x + tmp.y + tmp.z));
-}
-
-float	vec3_lenght_sq(t_vec3 a)
-{
-	return (vec3_sum(vec3_mul(a, a)));
-}
-
-float	vec3_sum(t_vec3 a)
-{
-	return (a.x + a.y + a.z);
 }
