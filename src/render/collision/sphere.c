a12a537408d0b50714782d7614cc11798d800bb3
Marwan Amine <maamine@3B4.42angouleme.fr>
2025-02-03 00:12:36 +0100

    Sphere better naming

--- src/render/collision/sphere.c
+++ src/render/collision/sphere.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 06:17:56 by tdelage           #+#    #+#             */
-/*   Updated: 2025/02/02 22:23:23 by maamine          ###   ########.fr       */
+/*   Updated: 2025/02/03 00:02:23 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,25 @@ bool	sphere_collide(struct s_ray *ray, struct s_sphere *sphere,
 	const float	radius = sphere->diameter / 2;
 	float		determinant;
 	t_vec3		ray_to_sphere;
-	float		dist_sq;
-	float		ray_off_center;
+	float		rtsp_dist_sq;
+	float		middle_dist;
 
 	ray_to_sphere = vec3_sub(sphere->base.position, ray->origin);
-	dist_sq = vec3_lenght_sq(ray_to_sphere);
-	ray_off_center = vec3_dot(ray->direction, ray_to_sphere);
-	determinant = powf(ray_off_center, 2) - (dist_sq - powf(radius, 2));
+	rtsp_dist_sq = vec3_lenght_sq(ray_to_sphere);
+	middle_dist = vec3_dot(ray->direction, ray_to_sphere);
+	determinant = powf(middle_dist, 2) - (rtsp_dist_sq - powf(radius, 2));
 	if (determinant < 0)
 		return (false);
 	if (!coll)
 		return (true);
 	determinant = sqrtf(determinant);
-	coll->dist = ray_off_center - determinant;
+	coll->dist = middle_dist - determinant;
 	if (coll->dist < 0.0f)
 		coll->dist += 2 * determinant;
 	coll->position = ray_collision(ray, coll->dist);
 	coll->normal = vec3_normalise(vec3_sub(coll->position,
 				sphere->base.position));
-	if (ray_off_center < determinant)
+	if (middle_dist < determinant)
 		coll->normal = vec3_scal_mul(coll->normal, -1.0f);
 	return (true);
 }
