fdb64237cecdd3ac57304d336f59198915d15ba2
Marwan Amine <maamine@3D6.42angouleme.fr>
2025-02-04 19:07:04 +0100

    Camera repaired

--- src/parsing/parse_camera.c
+++ src/parsing/parse_camera.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 06:23:52 by tdelage           #+#    #+#             */
-/*   Updated: 2025/02/04 16:44:47 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/02/04 19:06:00 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,22 @@ void	camera_create_transform(struct s_camera *camera)
 {
 	t_vec3	t_x;
 	t_vec3	t_y;
+	t_vec3	t_z;
 
-	t_x = vec3_normalise(vec3_cross(vec3_normalise(vec3_new(camera->direction.x,
-						camera->direction.y, 0)), vec3_new(0, 0, 1)));
-	t_y = vec3_normalise(vec3_cross(vec3_normalise(camera->direction), t_x));
+	t_z = vec3_normalise(camera->direction);
+	t_x = vec3_normalise(vec3_cross(
+				vec3_new(camera->direction.x, camera->direction.y, 0),
+				vec3_new(0, 0, 1)));
+	t_y = vec3_normalise(vec3_cross(camera->direction, t_x));
 	camera->transform.m00 = t_x.x;
 	camera->transform.m01 = t_x.y;
 	camera->transform.m02 = t_x.z;
 	camera->transform.m10 = t_y.x;
 	camera->transform.m11 = t_y.y;
 	camera->transform.m12 = t_y.z;
-	camera->transform.m20 = camera->direction.x;
-	camera->transform.m21 = camera->direction.y;
-	camera->transform.m22 = camera->direction.z;
+	camera->transform.m20 = t_z.x;
+	camera->transform.m21 = t_z.y;
+	camera->transform.m22 = t_z.z;
 	camera->inverse_transform = mat3_inverse(camera->transform);
 }
 
