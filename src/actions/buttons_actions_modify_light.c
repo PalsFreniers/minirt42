ceea336332dd338a69d82e1de62c65ef95e9b44d
Marwan Amine <maamine@3D6.42angouleme.fr>
2025-02-04 22:44:59 +0100

    Removing last llist vestige

--- src/actions/buttons_actions_modify_light.c
+++ src/actions/buttons_actions_modify_light.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 18:33:03 by maamine           #+#    #+#             */
-/*   Updated: 2025/02/04 20:38:51 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/02/04 22:44:07 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	button_add_light(struct s_mlx *mlx)
 	new = object_new(sizeof(struct s_light), OBJ_LIGHT);
 	if (!new)
 		return ;
-	ft_llist_push(&mlx->scene.map_lights, ft_llist_node_create(new));
 	scene_append(&(mlx->scene), new);
 	button_scene_modify_light(mlx, mlx->scene.len - 1);
 }
