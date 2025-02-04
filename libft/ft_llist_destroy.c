8a052ab78d64c0c88b55fed999a98ebf94ade89e
Marwan Amine <maamine@3D6.42angouleme.fr>
2025-02-04 21:05:19 +0100

    Simpler code

--- libft/ft_llist_destroy.c
+++ libft/ft_llist_destroy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_llist_destroy.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 20:08:28 by tdelage           #+#    #+#             */
-/*   Updated: 2024/05/10 20:12:05 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/02/04 20:47:33 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void	ft_llist_destroy(struct s_llist *list, t_lstdel_f del)
 {
-	while (list->head)
-		ft_llist_node_destroy(ft_llist_truncate(list), del);
+	struct s_llist_node	*node;
+
+	node = list->head;
+	while (node)
+	{
+		ft_llist_node_destroy(node, del);
+		node = node->next;
+	}
 	list->size = 0;
 	list->head = NULL;
 }
